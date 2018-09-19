39395638e447f525effd88062db46c64c33aed3d
Patrick Hartz <phartz@avarteq.de>
2018-09-19 10:17:48 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func sayTheAnswer(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("42"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+  http.HandleFunc("/", sayTheAnswer)
+  if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+    panic(err)
+  }
+}
